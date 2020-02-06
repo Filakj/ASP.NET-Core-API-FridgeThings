e0096a72fd4f17c70ef70500de3999f910e9324f
Rodney Canlas <canlasrj@gmail.com>
2020-02-05 21:25:47 -0600

    Started a ASP.Net web app project.

---
+++ WhatYouGot/WhatYouGot.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WhatYouGot.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
