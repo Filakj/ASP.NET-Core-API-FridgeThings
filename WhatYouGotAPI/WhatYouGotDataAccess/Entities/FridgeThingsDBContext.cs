a433dfc0505734e9ee414c8032e89f2507b216f8
Rodney Canlas <canlasrj@gmail.com>
2020-02-11 11:17:06 -0600

    Moved connection string to secrets.json

--- WhatYouGotAPI/WhatYouGotDataAccess/Entities/FridgeThingsDBContext.cs
+++ WhatYouGotAPI/WhatYouGotDataAccess/Entities/FridgeThingsDBContext.cs
@@ -24,11 +24,7 @@ namespace WhatYouGotDataAccess.Entities
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:fridgethings.database.windows.net,1433;Initial Catalog=FridgeThingsDB;Persist Security Info=False;User ID=FridgeManager;Password=FridgeThings!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
+            //Intentionally left blank.
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
