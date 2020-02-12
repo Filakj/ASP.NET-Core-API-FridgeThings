ed0fb32da17d8bf209541d1b05ef2ab2013bc561
filakj <john.m.filak@gmail.com>
2020-02-11 23:58:23 -0600

    swagger and cors added

--- WhatYouGotAPI/WhatYouGotAPI/Startup.cs
+++ WhatYouGotAPI/WhatYouGotAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WhatYouGotAPI
 {
@@ -19,18 +21,86 @@ namespace WhatYouGotAPI
         {
             Configuration = configuration;
         }
+        readonly string AllMyOrigins = "_allMyOrigins";
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-        }
+            //services.AddControllers();
+
+            services.AddControllers(options =>
+            {
+                /*
+                 By default, when the framework detects that the request is coming from a browser:
+                        The Accept header is ignored. The content is returned in JSON, unless otherwise configured*/
+                options.RespectBrowserAcceptHeader = true; // false by default
+            });
+
+
+
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Fridge Things API", Version = "v1" });
+            });
+
+
+
+
+
+
+            /*
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllMyOrigins, b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+            */
+
+            
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin();
+                });
+                options.AddPolicy(AllMyOrigins, builder =>
+                {
+                    builder.WithOrigins("http://localhost:44363")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+
+            
+
+            }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+            
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+            app.UseSwagger(options =>
+            {
+                options.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+            
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", swaggerOptions.Description);
+                //options.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
+            });
+            
+
+
+            app.UseCors(AllMyOrigins);
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
