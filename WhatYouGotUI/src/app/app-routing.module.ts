5dd8b48558c745168a725c34bcfbc62d6719b6a1
Matthew Liner <mattliner028@gmail.com>
2020-02-14 11:47:49 -0600

    Added some very basic routes to Navbar.

--- WhatYouGotUI/src/app/app-routing.module.ts
+++ WhatYouGotUI/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipeComponent } from './recipe/recipe.component';
 import { InstructionsComponent } from './instructions/instructions.component';
 import { ReviewComponent } from './review/review.component';
+import { ReviewListComponent } from './review-list/review-list.component';
+import { FavoritesListComponent } from './favorites-list/favorites-list.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 const routes: Routes = [
   { path: 'recipe', component: RecipeComponent },
   { path: 'instructions', component: InstructionsComponent },
-  { path: 'review', component: ReviewComponent }
+  { path: 'review', component: ReviewComponent },
+  { path: 'reviews', component: ReviewListComponent },
+  { path: 'favorites', component: FavoritesListComponent },
+  { path: 'recipes', component: RecipeListComponent }
 ];
 
 
