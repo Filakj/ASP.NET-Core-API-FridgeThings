97b21c82fa8d03510e974a9586b17c2904572d9b
Matthew Liner <mattliner028@gmail.com>
2020-02-14 10:42:11 -0600

    Added navbar,recipe,review,instructions components

--- WhatYouGotUI/src/app/app.module.ts
+++ WhatYouGotUI/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ReviewComponent } from './review/review.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RecipeComponent,
+    ReviewComponent,
+    InstructionsComponent,
   ],
   imports: [
     BrowserModule
