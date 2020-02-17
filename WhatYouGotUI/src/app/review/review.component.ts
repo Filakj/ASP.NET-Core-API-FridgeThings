1827e9efcfe230bd5f49f877f895edd4138c9ec3
Matthew Liner <mattliner028@gmail.com>
2020-02-17 12:26:24 -0600

    Post Review logic added; issues with testing.

--- WhatYouGotUI/src/app/review/review.component.ts
+++ WhatYouGotUI/src/app/review/review.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReviewService } from '../Services/fridgethingsServices/review.service';
+import { Review } from '../Models/fridgethingsModels/review';
+
 
 @Component({
   selector: 'app-review',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private reviewService: ReviewService) { }
 
   ngOnInit(): void {
+  }
+
+  addReview(userId: number, recipeId: number, rating: number, comment: string) {
+    var review: Review = {id: 1001, userId: userId, recipeId: recipeId, rating: rating, comment: comment}
+
+    this.reviewService.postReview(review);
   }
 
 }
