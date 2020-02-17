3291b406825712a5bf2fe3950f677347c24caba6
Rodney Canlas <canlasrj@gmail.com>
2020-02-17 15:07:14 -0600

    Added unit test for API.

---
+++ WhatYouGotAPI/WhatYouGotTest/Library/Models/IngredientTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WhatYouGotLibrary.Models;
+using Xunit;
+
+namespace WhatYouGotTest.Library.Models
+{
+    public class IngredientTest
+    {
+        private readonly Ingredient _ingredient = new Ingredient();
+
+        [Fact]
+        public void Id_NonEmptyValue_StoresCorrectly()
+        {
+            int randomIntValue = 1;
+            _ingredient.Id = randomIntValue;
+            Assert.Equal(randomIntValue, _ingredient.Id);
+        }
+
+        [Fact]
+        public void RecipeId_NonEmptyValue_StoresCorrectly()
+        {
+            int randomIntValue = 2;
+            _ingredient.RecipeId = randomIntValue;
+            Assert.Equal(randomIntValue, _ingredient.RecipeId);
+        }
+
+        [Fact]
+        public void ImageUrl_NonEmptyValue_StoresCorrectly()
+        {
+            string randomStringValue = "random.com";
+            _ingredient.ImageUrl = randomStringValue;
+            Assert.Equal(randomStringValue, _ingredient.ImageUrl);
+        }
+
+        [Fact]
+        public void IngredientName_NonEmptyValue_StoresCorrectly()
+        {
+            string randomStringValue = "chicken";
+            _ingredient.IngredientName = randomStringValue;
+            Assert.Equal(randomStringValue, _ingredient.IngredientName);
+        }
+
+        [Fact]
+        public void Amount_NonEmptyValue_StoresCorrectly()
+        {
+            decimal randomDecimalValue = Convert.ToDecimal(2.5);
+            _ingredient.Amount = randomDecimalValue;
+            Assert.Equal(randomDecimalValue, _ingredient.Amount);
+        }
+
+        [Fact]
+        public void Unit_NonEmptyValue_StoresCorrectly()
+        {
+            string randomStringValue = "lbs";
+            _ingredient.Unit = randomStringValue;
+            Assert.Equal(randomStringValue, _ingredient.Unit);
+        }
+    }
+}
---
+++ WhatYouGotAPI/WhatYouGotTest/Library/Models/ReviewTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WhatYouGotLibrary.Models;
+using Xunit;
+
+namespace WhatYouGotTest.Library.Models
+{
+    public class ReviewTest
+    {
+        private readonly Review _review = new Review();
+
+        [Fact]
+        public void Id_NonEmptyValue_StoresCorrectly()
+        {
+            int randomIntValue = 1;
+            _review.Id = randomIntValue;
+            Assert.Equal(randomIntValue, _review.Id);
+        }
+
+        [Fact]
+        public void UserId_NonEmptyValue_StoresCorrectly()
+        {
+            int randomIntValue = 1;
+            _review.UserId = randomIntValue;
+            Assert.Equal(randomIntValue, _review.UserId);
+        }
+
+        [Fact]
+        public void RecipeId_NonEmptyValue_StoresCorrectly()
+        {
+            int randomIntValue = 1;
+            _review.RecipeId = randomIntValue;
+            Assert.Equal(randomIntValue, _review.RecipeId);
+        }
+
+        [Fact]
+        public void Rating_NonEmptyValue_StoresCorrectly()
+        {
+            int randomIntValue = 1;
+            _review.Rating = randomIntValue;
+            Assert.Equal(randomIntValue, _review.Rating);
+        }
+
+        [Fact]
+        public void Comment_NonEmptyValue_StoresCorrectly()
+        {
+            string randomStringValue = "Wow! Delicious!";
+            _review.Comment = randomStringValue;
+            Assert.Equal(randomStringValue, _review.Comment);
+        }
+    }
+}
